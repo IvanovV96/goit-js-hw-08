71729cd91927bd2b374e3cc7a0ed46e6927ea6db
Vitaliy Ivanov <dotaivanov@gmail.com>
2022-08-14 14:04:20 +0300

    task 2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from "@vimeo/player"
+import throttle from 'lodash.throttle'
+const iframe = document.querySelector('iframe')
+const player = new Player(iframe)
+const currentSeconds = JSON.parse(localStorage.getItem("videoplayer-current-time")).seconds
+
+
+
+const onTimeSave = function (data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data))
+}
+
+player.on('timeupdate', throttle(onTimeSave, 1000))
+
+player.setCurrentTime(currentSeconds)
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from "lodash.throttle"
+
+const form = document.querySelector('.feedback-form')
+const email = form.email
+const message = form.message
+
+function onFormInput(evt) {
+    const formData =  {
+        email: email.value, 
+        message: message.value
+    }
+    const formDataStringified = JSON.stringify(formData)
+    localStorage.setItem("feedback-form-state", formDataStringified)
+}
+
+const throttledOnFormInput = throttle(onFormInput, 500)
+
+function onFormSubmit(evt) {
+    evt.preventDefault()
+    console.log({
+        email: email.value,
+        message: message.value
+    })
+    evt.currentTarget.reset()
+    localStorage.clear()
+}
+
+function populateFormInputs() {
+    const savedInputs = JSON.parse(localStorage.getItem("feedback-form-state"))
+    if(savedInputs) {
+        email.value = savedInputs.email
+        message.value = savedInputs.message
+    }
+}
+
+
+populateFormInputs()
+
+form.addEventListener('input', throttledOnFormInput)
+
+form.addEventListener('submit', onFormSubmit)
+
+
+
+
 
