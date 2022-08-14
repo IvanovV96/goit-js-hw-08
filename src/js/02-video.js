951c3b4539c6986e008b7374714ab3dd7c00d531
Vitaliy Ivanov <dotaivanov@gmail.com>
2022-08-14 16:16:38 +0300

    fixed ref error

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,17 +2,16 @@ import Player from "@vimeo/player"
 import throttle from 'lodash.throttle'
 const iframe = document.querySelector('iframe')
 const player = new Player(iframe)
-const STORAGE_KEY = "videoplayer-current-time"
 
 function setCurrentTime() {
-    const parsedCurrentTime = JSON.parse(localStorage.getItem(STORAGE_KEY))
+    const parsedCurrentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"))
     if(parsedCurrentTime) {
         player.setCurrentTime(parsedCurrentTime.seconds)
     }
 }
 
 function onTimeSave(data) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data))
 }
 
 player.on('timeupdate', throttle(onTimeSave, 1000))
