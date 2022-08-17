b9aed8c41c53d6f7c9b04fe88415173aca63acf5
Vitaliy Ivanov <dotaivanov@gmail.com>
2022-08-17 18:51:03 +0300

    added validation for empty string

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,12 +4,10 @@ const form = document.querySelector('.feedback-form')
 const email = form.email
 const message = form.message
 const STORAGE_KEY = "feedback-form-state"
+const formData = {}
 
 function onFormInput(evt) {
-    const formData =  {
-        email: email.value, 
-        message: message.value
-    }
+    formData[evt.target.name] = evt.target.value
     const formDataStringified = JSON.stringify(formData)
     localStorage.setItem(STORAGE_KEY, formDataStringified)
 }
@@ -18,10 +16,11 @@ const throttledOnFormInput = throttle(onFormInput, 500)
 
 function onFormSubmit(evt) {
     evt.preventDefault()
-    console.log({
-        email: email.value,
-        message: message.value
-    })
+    if(email.value === '' || message.value === '') {
+        console.log('Fill all inputs, please')
+    } else {
+    console.log(formData)
+    }
     evt.currentTarget.reset()
     localStorage.removeItem(STORAGE_KEY)
 }
@@ -29,7 +28,7 @@ function onFormSubmit(evt) {
 function populateFormInputs() {
     const savedInputs = JSON.parse(localStorage.getItem(STORAGE_KEY))
     if(savedInputs) {
-        email.value = savedInputs.email
+        email.value = savedInputs.email,
         message.value = savedInputs.message
     }
 }
